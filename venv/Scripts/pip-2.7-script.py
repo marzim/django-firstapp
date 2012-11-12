4ef6038e654cff6ad702a1bf57118e22f655adfc
marzim <marvin.casagnap@gmail.com>
2012-11-12 16:49:35 +0800

    my django app

---
+++ venv/Scripts/pip-2.7-script.py
@@ -0,0 +1,10 @@
+#!c:\mc185104\projects\python\django\django1.6\hellodjango\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.2.1','console_scripts','pip-2.7'
+__requires__ = 'pip==1.2.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('pip==1.2.1', 'console_scripts', 'pip-2.7')()
+    )
